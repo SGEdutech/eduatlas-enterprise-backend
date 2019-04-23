dbc5544864a564c205d07464ea1eac296b31f563
16bitash <entropy.ash@gmail.com>
2019-04-23 09:25:47 +0530

    Increase request header size to 100mb

--- server.js
+++ server.js
@@ -70,8 +70,9 @@ app.get('/excel-template/add-score', (req, res) => res.sendFile(path.join(__dirn
 app.get('/excel-template/add-student', (req, res) => res.sendFile(path.join(__dirname, 'excel-templates', 'add-student.csv')));
 app.get('/excel-template/mark-attendance', (req, res) => res.sendFile(path.join(__dirname, 'excel-templates', 'mark-attendance.csv')));
 
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({
+	limit: '100mb',
 	extended: true
 }));
 
